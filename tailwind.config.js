2de0e8aca8fad0899ae69be574aaffc82a8dc797
Sahwag546 <sahwagkantiwal494@gmail.com>
2024-09-12 09:27:56 +0530

    add nav

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'fira-sans': ["Fira Sans", "sans-serif"],
+        'exo-2': ["Exo 2", "system-ui"]
+      },
+      backgroundImage: {
+        'hero-image': "url('../src/images/bg-img.png')",
+      },
+    },
+  },
+  plugins: [],
+}
+
